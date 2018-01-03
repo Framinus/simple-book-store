44529fc429c04b89b17aa04ddeef55bde8346dfb
Mateo Balcorta <misterrattail@gmail.com>
2018-01-03 12:48:03 -0800

    Add body parser.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const routes = require('./routes')
 const app = express();
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.json())
 app.use('/', routes)
 
 const port = process.env.PORT || 3000;
