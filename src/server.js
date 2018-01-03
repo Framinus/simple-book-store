b25eabe53a7971eb61ce3c9f677acfde6428c798
Mateo Balcorta <misterrattail@gmail.com>
2018-01-03 12:31:36 -0800

    Remove get route.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,11 @@
 const express = require('express');
-
+const routes = require('./routes')
 const app = express();
+
+app.use('/', routes)
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  // res.send("hello");
-  res.json([
-    {
-      id: 1,
-      username: "samsepi0l",
-    },
-    {
-      id: 2,
-      username: "D0loresH4ze",
-    },
-  ]);
-});
 
 app.listen(port, () => {
   console.log(`The app is listening on port ${port}`);
